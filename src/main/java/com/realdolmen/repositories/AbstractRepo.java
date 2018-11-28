1e6618ac4ecedc0a7eddd548a5f29368a1422971
L3RY5 <lerys91@hotmail.com>
2018-11-28 11:32:23 +0100

    Merge origin/master
    Conflicts:
    	pom.xml

---
+++ src/main/java/com/realdolmen/repositories/AbstractRepo.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.realdolmen.repositories;
+
+/**
+ *
+ * @author YLOBL55
+ */
+public class AbstractRepo {
+    
+    //connecteren Met database
+    
+    //methode voor read
+    public void readCsvFile(){
+    }
+    
+    
+    //methode Voor Write
+    public void writeCsvFile(){
+        
+    }
+    
+    //methode print
+    public void printCsvFile(){
+    }
+    
+    
+    
+    //methode update
+    public void updataCsvFile(){
+    }
+    
+    
+    
+    
+}
---
+++ src/main/java/com/realdolmen/services/CsvService.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.realdolmen.services;
+
+/**
+ *
+ * @author YLOBL55
+ */
+public class CsvService {
+    
+}
